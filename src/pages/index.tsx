4169a15fbeef53b5fb62a3cba80d000b3b2f28dc
Jannis Jahr <jannis.jahr@hotmail.de>
2023-01-25 19:29:03 +0100

    add category scrolling bar on home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,19 +18,19 @@ const Home: NextPage = () => {
       <NavigationBar />
       <main className="flex min-h-screen flex-col items-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
         <div className="container flex flex-col justify-center gap-12 px-4 py-16 ">
-          <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
+          <h1 className="text-sm font-extrabold tracking-tight text-white sm:text-[2rem]">
             Browse <span className="text-[hsl(280,100%,70%)]">categories</span>
           </h1>
-          <section className="text-gray-400 bg-gray-900 body-font">
+          <section className="text-gray-400 bg-gray-900 rounded-xl shadow-md body-font">
             <div className="container px-5 py-24 mx-auto">
-              <div className="flex flex-wrap -m-4">
+              <div className="flex flex-shrink-0 overflow-scroll -m-4">
                 {categories.data?.map(category => (
-                  <CategoryTile key={category.id} category={category}></CategoryTile>
+                  <CategoryTile key={category.id} category={category} className="w-96"></CategoryTile>
                 ))}
               </div>
             </div>
           </section>
-          <h1 className="text-xl tracking-tight text-white sm:text-[5rem]">
+          <h1 className="text-xl tracking-tight text-white sm:text-[2rem]">
             Newest <span className="text-[hsl(280,100%,70%)]">items</span>
           </h1>
         </div >
@@ -73,19 +73,19 @@ const NavigationBar: React.FC = () => {
   )
 }
 
-const CategoryTile: React.FC<{ category: Category }> = ({ category }) => {
+const CategoryTile: React.FC<{ category: Category, className?: string }> = ({ category, className }) => {
   return (
-    <div className="p-4 lg:w-1/3">
-      <div className="h-full bg-gray-800 px-8 pt-16 pb-24 rounded-lg overflow-hidden text-center relative">
+    <div className={`p-4 flex ${className || ''}`}>
+      <div className="h-full flex flex-col items-center justify-between w-96 bg-gray-800 px-8 pt-16 pb-24 rounded-lg overflow-hidden text-center relative">
         <h2 className="tracking-widest text-xs title-font font-medium text-gray-500 mb-1">CATEGORY</h2>
         <h1 className="title-font sm:text-2xl text-xl font-medium text-white mb-3">{category.name}</h1>
-        <p className="leading-relaxed mb-3">Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing tousled waistcoat.</p>
-        <a className="text-indigo-400 inline-flex items-center">Learn More
+        <p className="leading-relaxed mb-3">{category.description}</p>
+        <button className="text-indigo-400 flex items-center">Check it out
           <svg className="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
             <path d="M5 12h14"></path>
             <path d="M12 5l7 7-7 7"></path>
           </svg>
-        </a>
+        </button>
       </div>
     </div>
   )
